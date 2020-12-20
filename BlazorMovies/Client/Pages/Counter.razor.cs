c8736c9b8000d106d3508a9cf1129b88e8577973
conclancy <conclancy@hotmail.com>
2020-12-20 15:26:15 -0500

    Add state to application.

--- BlazorMovies/Client/Pages/Counter.razor.cs
+++ BlazorMovies/Client/Pages/Counter.razor.cs
@@ -1,19 +1,27 @@
 ﻿using BlazorMovies.Shared.Entities;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static BlazorMovies.Client.Shared.MainLayout;
 
 namespace BlazorMovies.Client.Pages
 {
     public partial class Counter
     {
-        [Inject] 
+        [Inject]
         SingletonService singleton { get; set; }
 
-        [Inject] 
+        [Inject]
         TransietService transient { get; set; }
+
+        [Inject]
+        IJSRuntime js { get; set; }
+
+        [CascadingParameter]
+        public AppState AppState {get; set;} 
 
         private List<Movie> movies;
 
