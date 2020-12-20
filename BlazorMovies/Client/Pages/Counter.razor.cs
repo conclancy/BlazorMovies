18bdf05977c3a49e632b8764fc97a7ea5fe78566
conclancy <conclancy@hotmail.com>
2020-12-20 17:29:47 -0500

    Update Routes

--- BlazorMovies/Client/Pages/Counter.razor.cs
+++ BlazorMovies/Client/Pages/Counter.razor.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using static BlazorMovies.Client.Shared.MainLayout;
+using MathNet.Numerics.Statistics;
 
 namespace BlazorMovies.Client.Pages
 {
