df9e9f37552d280c363cb84b57b519ea8dde130c
conclancy <conclancy@hotmail.com>
2020-12-21 17:16:53 -0500

    Add Movie/Genre relationsip.
    Create a new relational object to populate a
    multi selctor box that allows a many-to-many
    relationship to exists between
    the Movie class and the Genre class.

--- BlazorMovies/Shared/Entities/Movie.cs
+++ BlazorMovies/Shared/Entities/Movie.cs
@@ -8,10 +8,14 @@ namespace BlazorMovies.Shared.Entities
 {
     public class Movie
     {
-        public int Id { get; set; } = 1;
-        public string  Title { get; set; }
-        public DateTime ReleaseDate { get; set; }
+        public int Id { get; set; }
+        [Reequired] public string  Title { get; set; }
+        public string Summary { get; set; }
+        public bool InTheaters { get; set; }
+        public string Trailer { get; set; }
+        [Reequired] public DateTime? ReleaseDate { get; set; }
         public string Poster { get; set; }
+        public List<MoviesGenres> MoviesGenres { get; set; } = new List<MoviesGenres>();
         public string TitleBrief
         {
             get
