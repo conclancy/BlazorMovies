c01ce9ccd7181eb08c5cffacd6cacc3e1d177663
conclancy <conclancy@hotmail.com>
2020-12-21 22:19:17 -0500

    Add Genre creation service.
    Wired up the genre creation service to add new
    Genres to the SQL Server database.

---
+++ BlazorMovies/Client/Helpers/HttpResponseWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public class HttpResponseWrapper<T>
+    {
+        public bool Sucess { get; set; }
+        public T Response { get; set; }
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+
+        public HttpResponseWrapper(T response, bool success, HttpResponseMessage httpResponseMessage) 
+        {
+            Sucess = success;
+            Response = response;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        /// <summary>
+        /// A method to access error text when the Http Request is not sucessful.
+        /// </summary>
+        /// <returns>string</returns>
+        public async Task<string> GetBody()
+        {
+            return await HttpResponseMessage.Content.ReadAsStringAsync();
+        }
+    }
+}
