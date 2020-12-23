a14ec1a5f2ea506cbf0611a97c929595f4f19e06
conclancy <conclancy@hotmail.com>
2020-12-23 14:15:49 -0500

    Add http service extension method class.
    Add a class that contains http service extension
    method for obtaining generic class objects.

---
+++ BlazorMovies/Client/Helpers/IHttpServiceExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public static class IHttpServiceExtensionMethods
+    {
+        public static async Task<T> GetHelper<T>(this IHttpService httpSerivce, string url)
+        {
+            var response = await httpSerivce.Get<T>(url);
+            if (!response.Sucess)
+            {
+                throw new ApplicationException(await response.GetBody());
+            }
+
+            return response.Response;
+        }
+    }
+}
--- BlazorMovies/Client/Repository/MoviesRepository.cs
+++ BlazorMovies/Client/Repository/MoviesRepository.cs
@@ -19,39 +19,22 @@ namespace BlazorMovies.Client.Repository
         }
 
         /// <summary>
-        /// Method for creating a Movie GET request to the server
-        /// </summary>
-        /// <typeparam name="T">Generic Object</typeparam>
-        /// <param name="url">url for movies in the server</param>
-        /// <returns>Object type requested</returns>
-        private async Task<T> Get<T>(string url)
-        {
-            var response = await httpService.Get<T>(url);
-            if (!response.Sucess)
-            {
-                throw new ApplicationException(await response.GetBody());
-            }
-
-            return response.Response;
-        }
-
-        /// <summary>
-        /// Utilize this class' private GET method to request an IndexPageDTO  
+        /// Utilize the IHttpServiceExtensionMethods GET helper to request an IndexPageDTO  
         /// </summary>
         /// <returns>IndexPageDTO</returns>
         public async Task<IndexPageDTO> GetIndexPageDTO()
         {
-            return await Get<IndexPageDTO>(url);
+            return await httpService.GetHelper<IndexPageDTO>(url);
         }
 
         /// <summary>
-        /// Utilize this class' private GET method to request a Movies Detail DTO
+        /// Utilize the IHttpServiceExtensionMethods GET helper to request a Movies Detail DTO
         /// </summary>
         /// <param name="id">MovieId</param>
         /// <returns>DetailsMovieDTO</returns>
         public async Task<DetailsMovieDTO> GetDetailsMovieDTO(int id)
         {
-            return await Get<DetailsMovieDTO>($"{url}/{id}");
+            return await httpService.GetHelper<DetailsMovieDTO>($"{url}/{id}");
         }
 
         public async Task<int> CreateMovie(Movie movie)
