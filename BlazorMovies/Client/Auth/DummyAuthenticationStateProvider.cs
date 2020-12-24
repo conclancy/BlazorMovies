1a5c5b665f7ba8026c4f2d3aadd82948182a5083
conclancy <conclancy@hotmail.com>
2020-12-24 13:36:57 -0500

    Add authentication.
    Add authentication service and decorators to components.  A dummy Authentication State Provider was also created to test and verify that the authentication system was working as expected; it will be removed in a future commit.

---
+++ BlazorMovies/Client/Auth/DummyAuthenticationStateProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Auth
+{
+    public class DummyAuthenticationStateProvider: AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonymous = new ClaimsIdentity(
+                new List<Claim>() { 
+                    new Claim(ClaimTypes.Name, "Connor"), 
+                    new Claim(ClaimTypes.Role, "Admin")
+                } 
+                , "test" //authentication type is required to view authenticated components.
+            );
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymous)));
+        }
+    }
+}
